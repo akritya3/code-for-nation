40cf8dfa48b205b7a00d0b10ab4229f2cff58a05
Anubhav Vats <anubhav.cse1999@gmail.com>
2020-06-18 23:33:05 +0530

    backend:initial

--- env.js
+++ env.js
@@ -1,16 +1,16 @@
 module.exports = {
   dbConfig: {
-    user: "onbit-syn",
-    password: "123",
-    database: "team_drive",
-    host: "localhost",
+    user: "codemagicians",
+    password: "codefornationIndia",
+    database: "naari",
+    host: "naari.cjpmgndx2osb.ap-south-1.rds.amazonaws.com",
     port: 5432,
     max: 20,
     connectionTimeoutMillis: 0,
     idleTimeoutMillis: 0
   },
-  COOKIE_SECRET: "#NIOFWNnonfs4n5-2n23mpo2",
-  SESSION_SECRET: "35nnn32nkls9023mlefNONWFE",
+  COOKIE_SECRET: "#NIOFWNdfdewnfs4n5-2n23mpo2",
+  SESSION_SECRET: "35nnn32newrew023mlefNONWFE",
   PORT: 5436,
   HOSTNAME:"localhost"
 };
