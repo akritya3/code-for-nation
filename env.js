b6c0228869901310dcc52dac624652efae6ca276
Anubhav Vats <anubhav.cse1999@gmail.com>
2020-06-18 23:27:26 +0530

    backend:initial

---
+++ env.js
@@ -0,0 +1,16 @@
+module.exports = {
+  dbConfig: {
+    user: "onbit-syn",
+    password: "123",
+    database: "team_drive",
+    host: "localhost",
+    port: 5432,
+    max: 20,
+    connectionTimeoutMillis: 0,
+    idleTimeoutMillis: 0
+  },
+  COOKIE_SECRET: "#NIOFWNnonfs4n5-2n23mpo2",
+  SESSION_SECRET: "35nnn32nkls9023mlefNONWFE",
+  PORT: 5436,
+  HOSTNAME:"localhost"
+};
--- index.js
+++ index.js
@@ -4,23 +4,45 @@ const morgan = require("morgan");
 const path = require("path");
 const http = require("http");
 const app  = express();
+const flash = require("connect-flash");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const {COOKIE_SECRET,SESSION_SECRET,PORT,HOSTNAME} = require("./env");
 
-const hostname = "localhost";
-const port = 5678;
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "views"));
 
-const server = http.createServer(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(morgan("dev"));
 
 
+app.use(cookieParser(COOKIE_SECRET));
+
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    maxAge : 24 * 60 * 60 * 1000
+  }
+}));
+
+
+app.use(flash());
+
+app.use(function (req, res, next) {
+  res.locals.success_message = req.flash("success_message");
+  res.locals.error_message = req.flash("error_message");
+  res.locals.error = req.flash("error");
+  next();
+});
+
 app.use(express.static(path.join(__dirname,"/public")));
 
 
+const server = http.createServer(app);
 
-
-
-
-server.listen(port,hostname,function(){
-  console.log("server is listening at http://localhost:5678");
+server.listen(PORT,HOSTNAME,function(){
+  console.log(`server is listening at http://${HOSTNAME}:${PORT}`);
 })
