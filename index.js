ae4624ab3935ea2166f9dce329b8b072e731c0e3
rishabhgarg25699 <rishabhgarg25699@gmail.com>
2020-06-20 12:10:09 +0530

    aadharUpdate

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const path = require("path");
 const http = require("http");
-const app  = express();
+const app = express();
 const flash = require("connect-flash");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
 const passport = require("passport");
-const {COOKIE_SECRET,SESSION_SECRET,PORT,HOSTNAME} = require("./env");
+const { COOKIE_SECRET, SESSION_SECRET, PORT, HOSTNAME } = require("./env");
 const db = require("./db");
 
 
-const {configStrategy,isAuthenticated} = require("./config/passport");
+const { configStrategy, isAuthenticated } = require("./config/passport");
 const user = require("./controller/user");
 const accountController = require("./controller/account");
 
-app.set("view engine","ejs");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 
@@ -31,7 +31,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   cookie: {
-    maxAge : 24 * 60 * 60 * 1000
+    maxAge: 24 * 60 * 60 * 1000
   }
 }));
 
@@ -48,17 +48,17 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.use(express.static(path.join(__dirname,"/public")));
-app.get("/login",user.getLogin);
-app.post("/login",user.postLogin);
-app.get("/signup",user.getSignup);
-app.post("/signup",user.postSignup);
-app.get("/account",isAuthenticated, accountController.getAccount);
-app.get("/logout",isAuthenticated, user.getLogout);
-
+app.use(express.static(path.join(__dirname, "/public")));
+app.get("/login", user.getLogin);
+app.post("/login", user.postLogin);
+app.get("/signup", user.getSignup);
+app.post("/signup", user.postSignup);
+app.get("/account", isAuthenticated, accountController.getAccount);
+app.get("/logout", isAuthenticated, user.getLogout);
+app.post("/updateaadhar", isAuthenticated, accountController.postAaadharUpdate);
 
 const server = http.createServer(app);
 
-server.listen(PORT,HOSTNAME,function(){
+server.listen(PORT, HOSTNAME, function () {
   console.log(`server is listening at http://${HOSTNAME}:${PORT}`);
 })
